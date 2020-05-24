2ebe2884d3f6d8d04a2f25f810148da889880c47
Nicol Acosta <nicolacosta8@gmail.com>
2020-05-24 18:46:17 -0500

    Add compiled to prod

---
+++ build/precache-manifest.60e1b33d032d20822e0e51cdefd13492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f077cee64f9a6f75de82e1d3e39194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "683d66701ba9314a5ed5",
+    "url": "/static/css/main.851d17e2.chunk.css"
+  },
+  {
+    "revision": "a5ed54987de09daf9c00",
+    "url": "/static/js/2.3feab0a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3feab0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683d66701ba9314a5ed5",
+    "url": "/static/js/main.965cff8e.chunk.js"
+  },
+  {
+    "revision": "7855de0cc3541ea33490",
+    "url": "/static/js/runtime-main.d6b8a280.js"
+  },
+  {
+    "revision": "37880436c2462131fb0b704e11667e60",
+    "url": "/static/media/Imagen1-620x264.37880436.png"
+  },
+  {
+    "revision": "4899c74abd3586f03b4410b7183ffc2c",
+    "url": "/static/media/background-phone.4899c74a.jpg"
+  }
+]);
